4b26d3d8d8865a530cd19fe23de39d0109317805
hamzabdel <hamzaabdel@csu.fullerton.edu>
2024-12-04 15:44:10 -0800

    working on grabbing gpu info

--- gpu_frame.py
+++ gpu_frame.py
@@ -1,10 +1,6 @@
 import customtkinter as ctk
-import psutil
-import platform
-import os
 import GPUtil
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.figure import Figure
+import subprocess
 
 darker_lightblue = "#468284"
 
@@ -21,11 +17,28 @@ def get_gpu_info():
         }
         return gpu_info
     else:
-        return {
-            "gpu_name": "Unknown GPU",
-            "memory_total": "N/A",
-            "memory_free": "N/A",
-            "memory_used": "N/A",
-            "gpu_temperature": "N/A",
-        }
+        print("Checking for other systems.")
+
+
+class GPUFrame(ctk.CTkFrame):
+    def __init__(self, parent):
+        super().__init__(parent, fg_color="white")
+
+        self.gpu_name_label = ctk.CTkLabel(self, text="GPU: ", font=("Segoe UI", 16))
+        self.gpu_name_label.grid(row=0, column=0, padx=10, pady=5)
+        
+        self.gpu_usage_label = ctk.CTkLabel(self, text="GPU Usage: 0%", font=("Segoe UI", 16, "italic"))
+        self.gpu_usage_label.grid(row=1, column=0, padx=10, pady=5)
+        
+        self.update_gpu_info()
+
+    def update_gpu_info(self):
+        gpu_info = get_gpu_info() 
+        
+        self.gpu_name_label.configure(text=f"GPU: {gpu_info['gpu_name']}")
+        self.gpu_usage_label.configure(text=f"GPU Temperature: {gpu_info['gpu_temperature']}°C")
+        
+        self.after(1000, self.update_gpu_info)
+        
+
 
