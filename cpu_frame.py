72b661dfe10021950833d7ae38a9aa4e8f427253
tliang3612 <tliang361@gmail.com>
2024-12-08 20:22:39 -0800

    Created Show Hide button

--- cpu_frame.py
+++ cpu_frame.py
@@ -4,9 +4,9 @@ import platform
 import os
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from winreg import OpenKey, QueryValueEx, HKEY_LOCAL_MACHINE
+import wmi
 
-if platform.system() == "Windows":
-    from winreg import OpenKey, QueryValueEx, HKEY_LOCAL_MACHINE
 
 darker_lightblue = "#4682B4"
 
@@ -17,15 +17,16 @@ def get_cpu_info():
         try:
             registry_key = OpenKey(HKEY_LOCAL_MACHINE, r"HARDWARE\DESCRIPTION\System\CentralProcessor\0") #get reference to first cpu registry key location
             cpu_name, reg = QueryValueEx(registry_key, "ProcessorNameString") #retrieve value of ProcessorNameString from the key to get the cpu name
-        except Exception:
+        except Exception as e:
             cpu_name = "Unknown CPU"
-    elif platform.system() == "Linux":
-        try:
-             with open("/proc/cpuinfo", "r") as f:
-                 for line in f:
-                     if "model name" in line:
-                         cpu_name = line.split(":")[1].strip()
-                         break
+    # untested
+    # elif platform.system() == "Linux":
+    #     try:
+    #         with open("/proc/cpuinfo", "r") as f:
+    #             for line in f:
+    #                 if "model name" in line:
+    #                     cpu_name = line.split(":")[1].strip()
+    #                     break
         except Exception:
             cpu_name = "Unknown CPU"
 
@@ -40,13 +41,47 @@ def get_cpu_info():
     }
 
 
+def get_cpu_temperature():
+    try:
+        temps = psutil.sensors_temperatures()
+        if "coretemp" in temps:
+            for entry in temps["coretemp"]:
+                if "Package id 0" in entry.label:
+                    return entry.current
+        elif "cpu_thermal" in temps:
+            return temps["cpu_thermal"][0].current
+        return "N/A"
+    except Exception as e:
+        return "N/A"
+
+
 class CPUFrame(ctk.CTkFrame):
     def __init__(self, parent):
         super().__init__(parent, fg_color="white")
 
-        self.grid_columnconfigure(0, weight=3)  # col 0 for cpu info
-        self.grid_columnconfigure(1, weight=0)  # col 1 for slider
-        self.grid_rowconfigure(4, weight=1)  # row 4 for graph
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=0)
+        self.grid_rowconfigure(1, weight=1)
+
+        # toggle cpu frame info
+        self.is_collapsed = False
+        self.toggle_button = ctk.CTkButton(
+            self,
+            text="Hide CPU Frame",
+            command=self.toggle_cpu_frame,
+            width=100,
+            fg_color="blue",
+            text_color="white",
+        )
+        self.toggle_button.grid(row=0, column=0, sticky="nw", padx=10, pady=5)
+
+        #contains the container for cpu frame contents
+        self.contents_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.contents_frame.grid_columnconfigure(0, weight=1)
+        self.contents_frame.grid_columnconfigure(1, weight=0)
+        self.contents_frame.grid_rowconfigure(5, weight=1)
+        self.contents_frame.grid(row=1, column=0, columnspan=2, sticky="nsew")
+
 
         # get info about the cpu
         cpu_info = get_cpu_info()
@@ -55,14 +90,15 @@ class CPUFrame(ctk.CTkFrame):
         self.cpu_usage_label = None
         self.cpu_name_label = None
         self.cpu_core_label = None
+        self.cpu_temp_text = None
+        self.cpu_temp_label = None
         self.create_cpu_labels(cpu_info)
 
         # cpu usage
-
         self.time_range = 60
         self.cpu_data = [0] * 3600
 
-        # create the plot
+        # create theplot
         self.figure = None
         self.ax = None
         self.line = None
@@ -73,48 +109,58 @@ class CPUFrame(ctk.CTkFrame):
         self.time_slider = None
         self.create_slider()
 
-        self.canvas = FigureCanvasTkAgg(self.figure, self)
-        self.canvas.get_tk_widget().grid(row=4, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+        self.canvas = FigureCanvasTkAgg(self.figure, self.contents_frame)
+        self.canvas.get_tk_widget().grid(row=5, column=0, columnspan=2,  sticky="nsew", padx=10, pady=10)
 
         self.update()
 
     def create_cpu_labels(self, cpu_info):
         # title label
         self.cpu_label = ctk.CTkLabel(
-            self,
+            self.contents_frame,
             text="CPU Usage Monitor",
             font=("Segoe UI", 18, "bold"),
             text_color="darkblue",
         )
-        self.cpu_label.grid(row=0, column=0, sticky="nw", padx=10, pady=10)
 
         # cpu usage label
         self.cpu_usage_text = ctk.StringVar()
         self.cpu_usage_label = ctk.CTkLabel(
-            self,
+            self.contents_frame,
             textvariable=self.cpu_usage_text,
             font=("Segoe UI", 16, "italic"),
             text_color="black",
         )
-        self.cpu_usage_label.grid(row=1, column=0, sticky="nw", padx=10, pady=5)
 
         # cpu name label
         self.cpu_name_label = ctk.CTkLabel(
-            self,
+            self.contents_frame,
             text=f"{cpu_info['cpu_name']}",
             font=("Segoe UI", 13, "normal"),
             text_color="blue",
         )
-        self.cpu_name_label.grid(row=2, column=0, sticky="nw", padx=10, pady=5)
 
         # cpu core count label
         self.cpu_core_label = ctk.CTkLabel(
-            self,
+            self.contents_frame,
             text=f"Cores: {cpu_info['cores']}, {cpu_info['logical_cores']} logical",
             font=("Segoe UI", 13, "italic"),
             text_color="gray",
         )
-        self.cpu_core_label.grid(row=3, column=0, sticky="nw", padx=10, pady=5)
+
+        # cpu temperature label
+        self.cpu_temp_text = ctk.StringVar()
+        self.cpu_temp_label = ctk.CTkLabel(
+            self.contents_frame,
+            textvariable=self.cpu_temp_text,
+            font=("Segoe UI", 13, "italic"),
+            text_color="green",
+        )
+        self.cpu_label.grid(row=0, column=0, sticky="nw", padx=10, pady=10)
+        self.cpu_usage_label.grid(row=1, column=0, columnspan = 1, sticky="nw", padx=10, pady=5)
+        self.cpu_name_label.grid(row=2, column=0, columnspan = 2,sticky="nw", padx=10, pady=5)
+        self.cpu_core_label.grid(row=3, column=0, columnspan = 2,sticky="nw", padx=10, pady=5)
+        self.cpu_temp_label.grid(row=4, column=0, columnspan = 2,sticky="nw", padx=10, pady=5)
 
     def create_plot(self):
         self.figure = Figure(figsize=(7, 2), dpi=90)
@@ -148,7 +194,7 @@ class CPUFrame(ctk.CTkFrame):
 
     def create_slider(self):
         # slider frame
-        slider_frame = ctk.CTkFrame(self, fg_color="transparent")
+        slider_frame = ctk.CTkFrame(self.contents_frame, fg_color="transparent")
         slider_frame.grid(row=0, column=1, rowspan=2, sticky="ne", padx=10, pady=5)
 
         # slider label
@@ -161,9 +207,9 @@ class CPUFrame(ctk.CTkFrame):
 
         # init the slider
         self.time_slider = ctk.CTkSlider(
-            slider_frame, from_=60, to=3600, number_of_steps=400, command=self.update_time_range
+            slider_frame, from_=60, to=3600, number_of_steps=60, command=self.update_time_range
         )
-        self.time_slider.set(60)  # Default to 60 seconds (1 minute)
+        self.time_slider.set(60)
         self.time_slider.pack(side="left", padx=5)
 
         # 60 mins label
@@ -177,15 +223,23 @@ class CPUFrame(ctk.CTkFrame):
 
     def update(self):
         # using psutil for cpu usage
+        self.update_cpu_info()
+        self.update_plot()
+        self.after(1000, self.update) # update each second using after. tkinter not thread safe
+
+    def update_cpu_info(self):
         new_data = psutil.cpu_percent(interval=0)
-        self.cpu_usage_text.set(f"{new_data:.1f}%")
+        self.cpu_usage_text.set(f"CPU Usage: {new_data:.1f}%")
 
         self.cpu_data.append(new_data)
         if len(self.cpu_data) > 60:
             self.cpu_data.pop(0)
 
-        self.update_plot()
-        self.after(1000, self.update) # update each second using after. tkinter not thread safe
+        cpu_temp = get_cpu_temperature()
+        if cpu_temp != "N/A":
+            self.cpu_temp_text.set(f"CPU Temp: {cpu_temp:.1f}°C")
+        else:
+            self.cpu_temp_text.set("CPU Temp: N/A")
 
     def update_plot(self):
         data_to_plot = self.cpu_data[-self.time_range:]
@@ -209,3 +263,15 @@ class CPUFrame(ctk.CTkFrame):
         self.ax.set_xticks(ticks)
 
         self.canvas.draw()
+
+    def toggle_cpu_frame(self):
+        if self.is_collapsed:
+            self.contents_frame.grid()
+            self.configure(fg_color="white")
+            self.toggle_button.configure(text="Hide CPU Frame")
+        else:
+            self.contents_frame.grid_remove()
+            self.configure(fg_color="transparent")
+            self.toggle_button.configure(text="Show CPU Frame")
+
+        self.is_collapsed = not self.is_collapsed
